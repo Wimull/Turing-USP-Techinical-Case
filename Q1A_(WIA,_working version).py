a7d44317ace32722098a8d4a1374a2429eb222f9
Wimull <aninhasilva@tutanota.com>
2022-03-29 13:19:56 -0300

    Fixed a bug where emailHandler wouldn't account for repeated char between emails 2 and 3, where 1 and 2 shared chars

--- Q1A_(WIA,_working version).py
+++ Q1A_(WIA,_working version).py
@@ -2,16 +2,19 @@ def conta_economia(emails):
     emailHandler = [];
     caracteresEconomizados = 0
     for conta in emails:
-        emailHandler.append(conta.split("@")[0]);
+        conta = conta.split("@")[0]
+        emailHandler.append(conta);
         if len(emailHandler) > 1:
-            if emailHandler[1][0] != emailHandler[0][0]: emailHandler.pop(0);
+            if conta[0] != emailHandler[0][0]: emailHandler.pop(0);
             else:
                 for caracteres in emailHandler[0]:
                     if emailHandler[1][0] == caracteres:
                         emailHandler[0] = emailHandler[0].split(caracteres, 1)[1];
                         emailHandler[1] = emailHandler[1].split(caracteres, 1)[1];
                         caracteresEconomizados += 1
-                emailHandler.pop()
+                emailHandler.clear()
+                emailHandler.append(conta)
+                
                     
 
 
@@ -24,4 +27,4 @@ def conta_economia(emails):
 
 
 
-print(conta_economia(["aninha123@usp.br","aninhazinha000@usp.br","jerson331@usp.br","jersaopika@usp.br","aninhaeudnv@usp.br"]))
+print(conta_economia(["aninha123@usp.br","aninhazinha000@usp.br","aninhazinhainha@usp.br","jerson331@usp.br","jersaopika@usp.br","aninhaeudnv@usp.br"]))
