3d6b0b9d9e7f64cd57c01efff980662bef3762f5
Wimull <aninhasilva@tutanota.com>
2022-03-31 15:38:58 -0300

    Added comments and fixed some styling issues

--- Q1A.py
+++ Q1A.py
@@ -1,30 +1,19 @@
 def conta_economia(emails):
-    emailHandler = [];
+    emailHandler = []      #Used in the comparison of two sequential emails by storing each of them and them comparing each letter from left to right
     caracteresEconomizados = 0
     for conta in emails:
         conta = conta.split("@")[0]
-        emailHandler.append(conta);
+        emailHandler.append(conta)                                 # Adds two emails to emailHandler and them compares each letter
         if len(emailHandler) > 1:
-            if conta[0] != emailHandler[0][0]: emailHandler.pop(0);
+            if conta[0] != emailHandler[0][0]: emailHandler.pop(0) #Looks if the second email can be trimmed and, if not, takes out the first one
             else:
                 for caracteres in emailHandler[0]:
-                    if emailHandler[1][0] == caracteres:
-                        emailHandler[0] = emailHandler[0].split(caracteres, 1)[1];
-                        emailHandler[1] = emailHandler[1].split(caracteres, 1)[1];
-                        caracteresEconomizados += 1
+                    if emailHandler[1][0] == caracteres: 
+                        emailHandler[1] = emailHandler[1].split(caracteres, 1)[1] #Takes out the first letter of the second email so that it can compare the next one
+                        caracteresEconomizados += 1                               #then counts the number of characters saved
                 emailHandler.clear()
-                emailHandler.append(conta)
+                emailHandler.append(conta)                         #Clears emailHandler and then adds the full version of the trimmed email, so as to compare it to the next email on the list
                 
-                    
+    return caracteresEconomizados
 
-
-    return caracteresEconomizados;
-
-
-
-
-
-
-
-
-print(conta_economia(["aninha123@usp.br","aninhazinha000@usp.br","aninhazinhainha@usp.br","jerson331@usp.br","jersaopika@usp.br","aninhaeudnv@usp.br"]))
+#print(conta_economia(["aninha123@usp.br","aninhazinha000@usp.br","aninhazinhainha@usp.br","jerson331@usp.br","jersaopika@usp.br","aninhaeudnv@usp.br"])) #expected output: 21
--- Q1B.py
+++ Q1B.py
@@ -4,19 +4,20 @@ def corrige_emails(emails):
     email_Joiner = ""
 
     for conta in emails:
-        if conta.find("@usp.br") == len(conta) - 7: emailsCorrigidos.append(conta)
+        if conta.find("@usp.br") == len(conta) - 7: emailsCorrigidos.append(conta) #Checks to see if the email isn't erroneous
         else:
             desvioHandler = []
-            chars = [c for c in conta]
+            chars = [c for c in conta]                                       #characters (chars) in the string
             chars.reverse()
-            desvio = (len(chars) - 1 - 6 - chars.index("@"))
+            desvio = (len(chars) - 7 - chars.index("@"))                     # "@"" should be inserted on 7th position from right to left, "desvio" checks for the deviation on that character position from right to left
             
             for i in range(0, (len(chars)))  :
-                desvioHandler.append(chars[i - desvio])
+                desvioHandler.append(chars[i - desvio])                      #With the deviation calculated, we can restore the original string by reading chars from the index given by "desvio"
 
             contaCorrigida = email_Joiner.join(desvioHandler)
-            if contaCorrigida.find("@usp.br") == -1: contaCorrigida = "ERRO" 
-            emailsCorrigidos.append(contaCorrigida)
+            if contaCorrigida.find("@usp.br") == -1: contaCorrigida = "ERRO" #Checks to see if there there wasn't another problem with the given email other than reversing and shifting
+            emailsCorrigidos.append(contaCorrigida)                          #Appends the corrected email if the process was successfull, and "ERRO" if not
     return emailsCorrigidos
 
-#print (corrige_emails( [ "aninha@usp.br", "ibol.alimacrb.psu@ocna", "t.alalalimacrb.repsu@ppo", ".orbmem_ovonrb.psu@gnirut"]))
+#print (corrige_emails( [ "aninha@usp.br", "ibol.alimacrb.psu@ocna", "t.alalalimacrb.repsu@ppo", ".orbmem_ovonrb.psu@gnirut"])) 
+#Expected output: ['aninha@usp.br', 'camila.lobianco@usp.br', 'ERRO', 'novo_membro.turing@usp.br']
